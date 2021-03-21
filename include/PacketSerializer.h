5a01df048450f064da3619f1dbd5c6ff373518fe
KilianTarb <kiliantarbett@gmail.com>
2021-03-21 10:46:25 +0800

    Add datagroup functions

--- include/PacketSerializer.h
+++ include/PacketSerializer.h
@@ -1,9 +1,18 @@
-
+#include <stdlib.h>
+#include <vector>
+#include "Types.h"
 
 class PacketSerializer
 {
     private:
+        vector<DataGroup> _data_groups;
+
     public:
         PacketSerializer();
         ~PacketSerializer();
+
+        vector<DataGroup *> GetDataGroups();
+        void AddDataGroup(DataGroup dg);
+        void AddDataGroup(unsigned int index, string body, size_t len);
+        vector<unsigned char *> GetBytes();
 };
